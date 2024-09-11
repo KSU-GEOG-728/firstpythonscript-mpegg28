931e6f9a9a6137a074572a947770a9c9c50827b4
mpegg28 <michaelpegg1794@gmail.com>
2024-09-11 14:40:41 -0500

    Update GitHub-FirstPythonScript.py
    Updated the First Python Script to met the requirements for the Exercise 5

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -3,9 +3,9 @@
 
 """
     File name: GitHub-FirstPythonScript.py
-    Author: ENTER YOUR  NAME
-    Description:  ENTER BRIEF DESCRIPTION
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT
+    Author: Michael Pegg
+    Description:  Calulates total river miles within the buffered ecoregion boundary.
+    Date created: 09/11/2024
     Python Version: 3.9.16
 """
 
@@ -14,3 +14,22 @@ import arcpy
 arcpy.env.overwriteOutput = True
 
 # Set current workspace
+arcpy.env.workspace = "C:/Users/mpegg/Documents/GitHub/firstpythonscript-mpegg28/GitHub-FirstPythonScript/ExerciseData.gdb"
+
+# Geoprocessing Starts Here:
+
+# Select the Ecoregion Polygon
+flinthills = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
+
+# Create a 10 km Buffer around the Ecoregion Boundary
+flinthillsbuffer = arcpy.analysis.Buffer(flinthills, "FlintHills10km", "10 Kilometer", 'FULL', 'ROUND',  'NONE',"", 'PLANAR')
+
+# Clip the Rivers inside the 10 km Buffer
+ClippedRivers = arcpy.analysis.Clip('ks_major_rivers', flinthillsbuffer,"ClippedRivers")
+
+# Adds the Length of the Rivers in Miles
+arcpy.management.AddGeometryAttributes(ClippedRivers, 'LENGTH', 'MILES_US')
+
+# Total Sum of the Length of the Rivers in Miles
+SumofRivers = arcpy.analysis.Statistics(ClippedRivers,"SumofRivers", [['LENGTH', "SUM"]])
+
