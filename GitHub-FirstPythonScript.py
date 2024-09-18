46fdd6562921949819087ba756178d19a9a681bb
Shawn Hutchinson <shutch@ksu.edu>
2024-09-18 08:21:21 -0500

    Instructor Corrections
    Original script ran to completion and generated correct results.
    
    I removed the use of two result objects.  These don't break your code, but are not needed or helpful...yet.

--- GitHub-FirstPythonScript.py
+++ GitHub-FirstPythonScript.py
@@ -22,13 +22,13 @@ arcpy.env.workspace = "C:/Users/mpegg/Documents/GitHub/firstpythonscript-mpegg28
 flinthills = arcpy.management.SelectLayerByAttribute('ks_ecoregions', 'NEW_SELECTION', "US_L3NAME = 'Flint Hills'")
 
 # Create a 10 km Buffer around the Ecoregion Boundary
-flinthillsbuffer = arcpy.analysis.Buffer(flinthills, "FlintHills10km", "10 Kilometer", 'FULL', 'ROUND',  'NONE',"", 'PLANAR')
+arcpy.analysis.Buffer(flinthills, "FlintHills10km", "10 Kilometer", 'FULL', 'ROUND',  'NONE',"", 'PLANAR')
 
 # Clip the Rivers inside the 10 km Buffer
-ClippedRivers = arcpy.analysis.Clip('ks_major_rivers', flinthillsbuffer,"ClippedRivers")
+arcpy.analysis.Clip('ks_major_rivers', "FlintHills10km", "ClippedRivers")
 
 # Adds the Length of the Rivers in Miles
-arcpy.management.AddGeometryAttributes(ClippedRivers, 'LENGTH', 'MILES_US')
+arcpy.management.AddGeometryAttributes("ClippedRivers", 'LENGTH', 'MILES_US')
 
 # Total Sum of the Length of the Rivers in Miles
 SumofRivers = arcpy.analysis.Statistics(ClippedRivers,"SumofRivers", [['LENGTH', "SUM"]])
